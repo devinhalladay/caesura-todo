c26362b631d308991c903a6eca7ce4836940da91
Devin Halladay <devin@devinhalladay.com>
2021-10-25 19:34:48 -0400

    Initial firebase and tailwind, tsc setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import initAuth from '../lib/auth';
+
+initAuth();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
